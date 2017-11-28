97aab820292b28d5087d4f4632583dde8ec259ca
GHOST home <lwhiteghostl@gmail.com>
2017-11-28 16:53:11 +0500

    Конструктор отчетов (дополнительные поля)

--- imports/ui/pages/ReportsPage.jsx
+++ imports/ui/pages/ReportsPage.jsx
@@ -7,20 +7,39 @@ import PivotTableUI from 'react-pivottable/PivotTableUI';
 import 'react-pivottable/pivottable.css'
 
 const derivedAttributes = {
-  "ФИО": (intern)=>{
+  "ФИО": (intern) => {
     return `${intern.lastname} ${intern.firstname} ${intern.middlename}`;
   },
-  "Дирекция": (intern)=>{
+  "Дирекция": (intern) => {
     return intern.direction ? intern.direction.name : "";
   },
-  "Наставник": (intern)=>{
+  "Наставник": (intern) => {
     return intern.tutor;
   },
-  "ВУЗ": (intern)=>{
+  "Табельный номер": (intern) => {
+    return intern.tabel;
+  },
+  "ВУЗ": (intern) => {
     let lastEducation = intern.educations[intern.educations.length - 1];
     return lastEducation ? lastEducation.university : "";
-  }
+  },
+  "Специальность": (intern) => {
+    let lastEducation = intern.educations[intern.educations.length - 1];
+    return lastEducation ? lastEducation.speciality : "";
+  },
+  "Кафедра": (intern) => {
+    let lastEducation = intern.educations[intern.educations.length - 1];
+    return lastEducation ? lastEducation.department : "";
+  },
+  "Курс": (intern) => {
+    let lastEducation = intern.educations[intern.educations.length - 1];
+    return lastEducation && lastEducation.course ? `${lastEducation.course} курс` : "";
+  },
 };
+
+const hiddenAttributes = [
+  "_id", "lastname", "firstname", "middlename", "direction", "educations", "tutor", "avatar", "tabel"
+];
 
 export default class ReportsPage extends BaseComponent {
   constructor(props) {
@@ -28,15 +47,22 @@ export default class ReportsPage extends BaseComponent {
     this.state = props;
   }
 
-  render(){
-    const { loading, listExists, interns } = this.props;
+  render() {
+    const {loading, listExists, interns} = this.props;
 
     if (!listExists) {
-      return <NotFoundPage />;
+      return <NotFoundPage/>;
     }
 
     return loading ?
-      <Message title={i18n.__('pages.ReportsPage.loading')} /> :
-      <PivotTableUI data={interns} derivedAttributes={derivedAttributes} onChange={s => this.setState(s)} {...this.state}/>
+        <Message title={i18n.__('pages.ReportsPage.loading')}/> :
+        <PivotTableUI
+            data={interns}
+            hiddenAttributes={hiddenAttributes}
+            hiddenFromAggregators={hiddenAttributes}
+            derivedAttributes={derivedAttributes}
+            onChange={s => this.setState(s)}
+            {...this.state}
+        />
   }
 }
